2d5657a3297626f5207dc065c4e50426196a0d8d
DenyWild <denyswildson2011@gmail.com>
2021-11-14 14:45:40 -0300

    Tabela endereço implementada com os 4 metodos

--- ProjetoSerralharia/src/main/java/com/example/demo/Controller/EnderecoController.java
+++ ProjetoSerralharia/src/main/java/com/example/demo/Controller/EnderecoController.java
@@ -1,5 +1,59 @@
 package com.example.demo.Controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.entities.Cliente;
+import com.example.demo.model.entities.Endereco;
+import com.example.demo.model.repositories.EnderecoRepository;
+
+@RestController
+@RequestMapping("/endereco")
 public class EnderecoController {
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Endereco salvarEndereco(@Valid Endereco endereco) {
+	enderecoRepository.save(endereco);
+	return endereco;
+}
+    /*@PutMapping
+    public Endereco alteraarEndereco(@Valid Endereco endereco) {
+    	enderecoRepository.save(endereco);
+    		return endereco;
+    }
+     */
+    @GetMapping
+    public Iterable<Endereco> mostrarEndereco(){
+    	return enderecoRepository.findAll();
+    	
+    }
+    
+    @GetMapping(path = "/{id_endereco}")
+    public Optional<Endereco> consultarEnderecoPorId(@PathVariable Integer id_endereco){
+    	return enderecoRepository.findById(id_endereco);
+    	
+    }
+    @DeleteMapping(path = "/{id_endereco}")
+    public void deletarEnderecoPorId(@PathVariable Integer id_endereco){
+    	enderecoRepository.deleteById(id_endereco);
+    	
+    }
+    
 }
--- ProjetoSerralharia/src/main/java/com/example/demo/model/entities/Endereco.java
+++ ProjetoSerralharia/src/main/java/com/example/demo/model/entities/Endereco.java
@@ -1,94 +1,197 @@
 package com.example.demo.model.entities;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name="endereco")
 public class Endereco {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	@Column(name="id_endereco")
+	private Integer id_endereco;
 	
+	@Column(name="rua", nullable = false)
+	@NotBlank(message = "Campo rua é obrigatório")
 	private String rua;
 	
+	@Column(name="bairro", nullable = false)
+	@NotBlank(message = "Campo bairro é obrigatório")
 	private String bairro;
 	
+	@Column(name="estado", nullable = false)
+	@NotBlank(message = "Campo estado é obrigatório")
 	private String estado;
 	
+	@Column(name="numero", nullable = false)
 	private int numero;
 	
+	@Column(name="cidade", nullable = false)
+	@NotBlank(message = "Campo cidade é obrigatório")
 	private String cidade;
 	
+	@Column(name="cep", nullable = false)
+	@NotBlank(message = "Campo cep é obrigatório")
+	@Size(min=8,max=8, message= "O cep tem que ter 8 caracteres")
 	private String cep;
 	
+	@Column(name="ponto_referencia", nullable = false)
+	@NotBlank(message = "Campo ponto de referencia é obrigatório")
 	private String pontRef;
 
-	public int getId() {
-		return id;
+	
+	
+	public Endereco() {
+	
+	
 	}
 
-	public void setId(int id) {
-		this.id = id;
+
+
+	public Endereco(String rua, String bairro, String estado, int numero, String cidade, String cep, String pontRef) {
+		super();
+		this.rua = rua;
+		this.bairro = bairro;
+		this.estado = estado;
+		this.numero = numero;
+		this.cidade = cidade;
+		this.cep = cep;
+		this.pontRef = pontRef;
 	}
+
+
+
+	public Integer getId_endereco() {
+		return id_endereco;
+	}
+
+
+
+	public void setId_endereco(Integer id_endereco) {
+		this.id_endereco = id_endereco;
+	}
+
+
 
 	public String getRua() {
 		return rua;
 	}
 
+
+
 	public void setRua(String rua) {
 		this.rua = rua;
 	}
+
+
 
 	public String getBairro() {
 		return bairro;
 	}
 
+
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+
+
 
 	public String getEstado() {
 		return estado;
 	}
 
+
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
+
 
 	public int getNumero() {
 		return numero;
 	}
 
+
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+
+
 
 	public String getCidade() {
 		return cidade;
 	}
 
+
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+
+
 
 	public String getCep() {
 		return cep;
 	}
 
+
+
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+
+
 
 	public String getPontRef() {
 		return pontRef;
 	}
 
+
+
 	public void setPontRef(String pontRef) {
 		this.pontRef = pontRef;
 	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bairro, cep, cidade, estado, id_endereco, numero, pontRef, rua);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(bairro, other.bairro) && Objects.equals(cep, other.cep)
+				&& Objects.equals(cidade, other.cidade) && Objects.equals(estado, other.estado)
+				&& Objects.equals(id_endereco, other.id_endereco) && Objects.equals(numero, other.numero)
+				&& Objects.equals(pontRef, other.pontRef) && Objects.equals(rua, other.rua);
+	}
 	
 	
+	
+
 }
